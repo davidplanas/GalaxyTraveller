52082160f40d948182ef9c7129f89afacf65db88
David Planas Noguer <49083907+davidplanas@users.noreply.github.com>
2019-11-18 19:40:16 +0100

    Pantalla introducción

--- app/src/main/java/com/example/galaxytraveller/p2_start.java
+++ app/src/main/java/com/example/galaxytraveller/p2_start.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class p2_start extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.p2_start);
+
+
+
     }
 }
