a1777c645672344b88fdaa47517ce7f6feeb6367
David Planas Noguer <49083907+davidplanas@users.noreply.github.com>
2019-11-18 21:44:59 +0100

    Pantalla dificultades

--- app/src/main/java/com/example/galaxytraveller/p2_start.java
+++ app/src/main/java/com/example/galaxytraveller/p2_start.java
@@ -16,12 +16,22 @@ public class p2_start extends AppCompatActivity {
         setContentView(R.layout.p2_start);
 
         Button p2_buttonInicio = (Button) findViewById(R.id.p2_buttonInicio);
+        Button p2_buttonJugar = (Button) findViewById(R.id.p2_buttonJugar);
 
         p2_buttonInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent p2_buttonInicio = new Intent(p2_start.this, p1_intro.class);
                 startActivity(p2_buttonInicio);
+            }
+
+        });
+
+        p2_buttonJugar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent p2_buttonJugar = new Intent(p2_start.this, p3_dificultad.class);
+                startActivity(p2_buttonJugar);
             }
 
         });
