3148f227cd40ff590514416a79b515434f51bcc3
David Planas Noguer <49083907+davidplanas@users.noreply.github.com>
2019-11-25 15:22:32 +0100

    menu niveles

---
+++ app/src/main/java/com/example/galaxytraveller/p4_menu_nivelesAdapter.java
@@ -0,0 +1,20 @@
+package com.example.galaxytraveller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class p4_menu_nivelesAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.p4_menu_niveles);
+
+
+    }
+}
