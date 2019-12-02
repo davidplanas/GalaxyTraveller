5a7e19c18e59a1a2b74b5f203b5636f57a215073
David Planas Noguer <49083907+davidplanas@users.noreply.github.com>
2019-12-02 19:43:42 +0100

    Reescalado de mis pantallas + tipografia

--- app/src/main/java/com/example/galaxytraveller/p2_start.java
+++ app/src/main/java/com/example/galaxytraveller/p2_start.java
@@ -3,6 +3,7 @@ package com.example.galaxytraveller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,14 +11,27 @@ import android.widget.Button;
 
 public class p2_start extends AppCompatActivity {
 
+    private Typeface fuenteApp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.p2_start);
 
-        Button p2_buttonInicio = (Button) findViewById(R.id.p2_buttonInicio);
+        String ruta_fuente = "fonts/MidNight.ttf";
+        this.fuenteApp = Typeface.createFromAsset(getAssets(), ruta_fuente);
+
         Button p2_buttonJugar = (Button) findViewById(R.id.p2_buttonJugar);
+        Button p2_buttonInicio = (Button) findViewById(R.id.p2_buttonInicio);
+        Button p2_buttonPersonPj = (Button) findViewById(R.id.p2_buttonPersonPj);
         Button p2_buttonRanking = (Button) findViewById(R.id.p2_buttonRanking);
+        Button p1_buttonAyuda = (Button) findViewById(R.id.p1_buttonAyuda);
+
+        p2_buttonJugar.setTypeface(fuenteApp);
+        p2_buttonInicio.setTypeface(fuenteApp);
+        p2_buttonPersonPj.setTypeface(fuenteApp);
+        p2_buttonRanking.setTypeface(fuenteApp);
+        p1_buttonAyuda.setTypeface(fuenteApp);
 
         p2_buttonInicio.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/galaxytraveller/p3_dificultad.java
+++ app/src/main/java/com/example/galaxytraveller/p3_dificultad.java
@@ -3,6 +3,7 @@ package com.example.galaxytraveller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,27 @@ import android.widget.Button;
 
 public class p3_dificultad extends AppCompatActivity {
 
+    private Typeface fuenteApp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.p3_dificultad);
 
+        String ruta_fuente = "fonts/MidNight.ttf";
+        this.fuenteApp = Typeface.createFromAsset(getAssets(), ruta_fuente);
+
         Button p3_buttonArrowBack = (Button) findViewById(R.id.p3_buttonArrowBack);
+        Button p3_buttonDifBaja = (Button) findViewById(R.id.p3_buttonDifBaja);
+        Button p3_buttonDifMedia = (Button) findViewById(R.id.p3_buttonDifMedia);
+        Button p3_buttonDifAlta = (Button) findViewById(R.id.p3_buttonDifAlta);
+        Button p1_buttonAyuda = (Button) findViewById(R.id.p1_buttonAyuda);
+
+        p3_buttonDifBaja.setTypeface(fuenteApp);
+        p3_buttonDifMedia.setTypeface(fuenteApp);
+        p3_buttonDifAlta.setTypeface(fuenteApp);
+        p1_buttonAyuda.setTypeface(fuenteApp);
+
 
         p3_buttonArrowBack.setOnClickListener(new View.OnClickListener() {
             @Override
